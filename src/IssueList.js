5510af18d5b12f4a4c3e17ad1854386a6d0be1e7
Ammy Kalsi <amrinder.kalsi13.com>
2023-06-09 11:58:21 -0400

    Added GraphQl Mutation for creating the issue

--- src/IssueList.js
+++ src/IssueList.js
@@ -8,10 +8,10 @@ class IssueList extends Component {
     this.state = {
       issues: []
     }
-    
   }
 
   componentDidMount() {
+    // GraphQl query for fetching the list of issues
     fetch('/graphql', { 
       method: 'POST',
       headers: {
@@ -45,40 +45,72 @@ class IssueList extends Component {
   }
 
   createIssue = (issue) => {
-    fetch('/api/issues', {
+    // GraphQL Mutation for creating the issue
+    fetch('/graphql', { 
       method: 'POST',
       headers: {
         'content-type': 'application/json'
       },
-      body: JSON.stringify(issue)
-    }).then(res => {
-      if (res.ok) {
-        res.json().then(data => {
-          console.log(data)
-          data.created = new Date(data.created);
-          if (data.completionDate) {
-            data.completionDate = new Date(data.completionDate);
+      body: JSON.stringify({
+        query: `mutation Mutation($issue: IssueInputs) {
+          issueAdd(issue: $issue) {
+            completionDate
+            created
+            effort
+            id
+            owner
+            status
+            title
           }
-          const newIssueArray = [...this.state.issues, data];
+        }`, 
+        variables: {issue}
+      })
+    })
+      .then(res => res.json())
+      .then(body => {
+        console.log(body);
+        body.data.issueAdd.created = new Date(body.data.issueAdd.created);
+          if (body.data.issueAdd.completionDate) {
+            body.data.issueAdd.completionDate = new Date(body.data.issueAdd.completionDate);
+          }
+          const newIssueArray = [...this.state.issues, body.data.issueAdd];
           this.setState({ issues: newIssueArray});
-        })
-      } else {
-        res.json().catch(err => {console.log(err)});
-      }
-    }).catch(error => console.log(error));
+      });
+
+    // Rest API POST call for creating the issue
+    // fetch('/api/issues', {
+    //   method: 'POST',
+    //   headers: {
+    //     'content-type': 'application/json'
+    //   },
+    //   body: JSON.stringify(issue)
+    // }).then(res => {
+    //   if (res.ok) {
+    //     res.json().then(data => {
+    //       console.log(data)
+    //       data.created = new Date(data.created);
+    //       if (data.completionDate) {
+    //         data.completionDate = new Date(data.completionDate);
+    //       }
+    //       const newIssueArray = [...this.state.issues, data];
+    //       this.setState({ issues: newIssueArray});
+    //     })
+    //   } else {
+    //     res.json().catch(err => {console.log(err)});
+    //   }
+    // }).catch(error => console.log(error));
   }
- 
     render() { 
-        return ( 
-            <div>
-                <h1>Issue Tracker</h1>
-                <IssueFilter />
-                <hr />
-                <IssueTable issues={this.state.issues}/>
-                <hr />
-                <IssueAdd createIssue={this.createIssue} />
-            </div>
-         );
+      return ( 
+          <div>
+              <h1>Issue Tracker</h1>
+              <IssueFilter />
+              <hr />
+              <IssueTable issues={this.state.issues}/>
+              <hr />
+              <IssueAdd createIssue={this.createIssue} />
+          </div>
+        );
     }
 }
  
